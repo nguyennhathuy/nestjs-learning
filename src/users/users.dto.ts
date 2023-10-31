4df25e082a9f92524543ba85ba67d622982b0d6f
Huy Nguyen <nhathuy120796@gmail.com>
2023-10-31 23:03:52 +0700

    daily commit 3

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsNumber } from "class-validator"
+
 export class UsersDTO {
+    @IsNotEmpty()
     username: string
+
+    @IsNotEmpty()
+    @IsNumber()
     age: number
+
+    @IsNotEmpty()
     city: string
 }
