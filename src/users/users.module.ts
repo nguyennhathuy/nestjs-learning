e0244a77e2bbd3f5b8804bd2de31008d544b1102
Huy Nguyen <nhathuy120796@gmail.com>
2023-10-31 23:03:58 +0700

    daily commit 4

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,16 @@
 import { Module } from "@nestjs/common"
 import { UsersController } from "./users.controller"
 import { UsersService } from "./user.service"
+import { PostsModule } from "src/posts/posts.module"
 
 
 @Module({
-    imports: [],
+    imports: [PostsModule.register()],
     controllers: [UsersController],
-    providers: [UsersService]
+    providers: [{
+        provide: "CLASS_CUSTOM",
+        useClass: UsersService
+    }]
 })
 
 export class UsersModule {}
