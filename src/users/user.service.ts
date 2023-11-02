6d833c02e67f929d7a4bc6ccb3f3a334799481aa
Huy Nguyen <nhathuy120796@gmail.com>
2023-11-02 23:08:58 +0700

    daily commit 1

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -9,6 +9,7 @@ export class UsersService {
     async sayHelloPost(): Promise<string> {
         return 'say hello post'
     }
+    
     async createUser(user: UsersDTO): Promise<string> {
         return `create success ! ${user.username}`
     }
